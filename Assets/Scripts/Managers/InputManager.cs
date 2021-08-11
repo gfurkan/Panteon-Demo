55f4afd30c2e171407496d07b120c154ee1fa25f
furkan <34320035+gfurkan@users.noreply.github.com>
2021-08-11 14:24:33 +0300

    Player Controls added.

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    #region Singleton
+    private static InputManager _Instance = null;
+    public static InputManager Instance
+    {
+        get
+        {
+            if (_Instance == null)
+            {
+                _Instance = new GameObject("InputManager").AddComponent<InputManager>();
+            }
+            return _Instance;
+        }
+    }
+    #endregion
+
+    private bool _touched = false;
+    private float _touchPos, _touchPosNext;
+    private float _direction;
+
+    #region Get Set
+
+    public float direction
+    {
+        get
+        {
+            return _direction;
+        }
+    }
+    public bool touched 
+    {
+        get
+        {
+            return _touched;
+        }
+    }
+
+    #endregion
+
+    private void Update()
+    {
+        Debug.Log(_direction);
+        if (Input.GetMouseButtonDown(0))
+        {
+            _touchPos = Input.mousePosition.x;
+            _touched = true;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            _touchPosNext = Input.mousePosition.x;
+            _direction = _touchPosNext - _touchPos;
+            _touchPos = _touchPosNext;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            _touched = false;
+            _direction = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Player Character/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody rb;
+    InputManager inputManager;
+
+    [SerializeField]
+    private double runningSpeed = 0,swerveSpeed=0;
+
+    private bool startRunning = false;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        inputManager = InputManager.Instance;
+    }
+
+    void Update()
+    {
+        if (inputManager.touched)
+        {
+            if (!startRunning)
+            {
+                StartRunning();
+            }
+        }
+        if (startRunning)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 1 * (float)runningSpeed) ;
+            PlayerControl(inputManager.direction);
+        }
+    }
+    void StartRunning()
+    {
+        animator.SetBool("Run", true);
+        animator.SetBool("Idle", false);
+        startRunning = true;
+    }
+    void PlayerControl(float touchDirection)
+    {
+        transform.position = new Vector3(transform.position.x + touchDirection * Time.deltaTime*(float)swerveSpeed, transform.position.y, transform.position.z);
+    }
+}
